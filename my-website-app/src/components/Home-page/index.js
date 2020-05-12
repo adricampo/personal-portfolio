fe514814ef680025a46a5b4d275381eeb1574a18
Adrian Campo Calvo <Adri@MacBook-Pro-de-Adrian.local>
2020-05-12 12:31:40 +0200

    added typewritter and modified styles

--- my-website-app/src/components/Home-page/index.js
+++ my-website-app/src/components/Home-page/index.js
@@ -1,17 +1,37 @@
 import React from 'react'
 import './index.sass'
+import Typewriter from 'typewriter-effect'
 export default function ({ onProjects }) {
     return <>
         <section className="home-page">
             <section className="myName">
-                {/* <div className="myName__background background">
-                    <img className="background__profile-photo" src="./img/profilePhoto.png"/>
-                </div> */}
                 <div className="myName__container container">
                     <div className="container__titles titles">
-                        <h2>Adri Campo · Junior Full-Stack Developer</h2>
-                        {/* <h4 className="titles__direction2">Junior Full-Stack Developer</h4> */}
-                        {/* <h5>living and learning in Badalona, Barcelona 😎✌🏻</h5> */}
+                    <h2>Hi I'm Adri Campo</h2>
+                    <Typewriter
+                        options={{
+                            strings: [`I'm a Junior Full Stack Developer.`, 
+                            `Based in Barcelona.`, 
+                            `Feel free to have a look to my site.`,
+                            `Now I'm looking for an opportunity as a Junior Developer.`,
+                            `If you are interested on my profile please contact me.`],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                        onInit={(typewriter) => {
+                            typewriter
+                            .typeString()
+                            .callFunction(() => {
+                                console.log('String typed out!');
+                            })
+                            .pauseFor(1500)
+                            .deleteAll()
+                            .callFunction(() => {
+                                console.log('All strings were deleted');
+                            })
+                            .start();
+                        }}
+                        />
                         <button className="titles__button" onClick={event => {
                             event.preventDefault()
 
