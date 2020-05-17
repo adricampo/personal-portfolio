bd3d18cb16226c1ca1e9619885c8982a1d731c7d
Adrian Campo Calvo <Adri@MacBook-Pro-de-Adrian.local>
2020-05-17 17:15:15 +0200

    modifications in the styles

--- my-website-app/src/components/Home-page/index.js
+++ my-website-app/src/components/Home-page/index.js
@@ -7,28 +7,28 @@ export default function ({ onProjects }) {
             <section className="myName">
                 <div className="myName__container container">
                     <div className="container__titles titles">
-                    <h2>Hi I'm Adri Campo</h2>
+                    <h2>Hi I'm <span className="span">Adri Campo</span></h2>
                     <Typewriter
                         options={{
-                            strings: [`I'm a Junior Full Stack Developer.`, 
+                            strings: 
+                            [
+                            `I'm a Junior Full Stack Developer.`, 
                             `Based in Barcelona.`, 
-                            `Feel free to have a look to my site.`,
-                            `Now I'm looking for an opportunity as a Junior Developer.`,
-                            `If you are interested on my profile please contact me.`],
+                            `Feel free to have a look to my website.`,
+                            `Currently I'm looking for an opportunity..`,
+                            `..as a Junior Web Developer.`,
+                            `If you're interested please contact me.`
+                            ],
                             autoStart: true,
                             loop: true,
                         }}
                         onInit={(typewriter) => {
                             typewriter
                             .typeString()
-                            .callFunction(() => {
-                                console.log('String typed out!');
-                            })
+                            
                             .pauseFor(1500)
                             .deleteAll()
-                            .callFunction(() => {
-                                console.log('All strings were deleted');
-                            })
+                           
                             .start();
                         }}
                         />
